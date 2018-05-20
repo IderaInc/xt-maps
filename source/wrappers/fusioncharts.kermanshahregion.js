dc421d170ee2c8591b698d96fccb7476bc1abb7a
arnab003 <arnab.saha@fusioncharts.com>
2018-05-20 06:23:37 +0530

    RED-7381: Update Iran

---
+++ source/wrappers/fusioncharts.kermanshahregion.js
@@ -0,0 +1,4 @@
+import kermanshahregion from '../maps/fusioncharts.kermanshahregion';
+
+FusionCharts.addDep(kermanshahregion);
+FusionCharts.addFile('fusioncharts.kermanshahregion.js');
---
+++ source/wrappers/fusioncharts.tehranregion.js
@@ -0,0 +1,4 @@
+import tehranregion from '../maps/fusioncharts.tehranregion';
+
+FusionCharts.addDep(tehranregion);
+FusionCharts.addFile('fusioncharts.tehranregion.js');
