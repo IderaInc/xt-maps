c7a4fe462122d3e2617607cd54bf34c81dba1f39
arnab003 <arnab.saha@fusioncharts.com>
2018-05-23 18:49:19 +0530

    RED-7547: Modify webpack config to generate wrapper files

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const path = require('path'),
   WrapperPlugin = require('wrapper-webpack-plugin');
 
 let IS_DEV = process.env.NODE_ENV === 'development',
-  sourceLocation = process.env.NODE_ENV === 'custom' ? './custom/wrappers/' : './source/wrappers/',
+  sourceLocation = process.env.NODE_ENV === 'custom' ? './custom/maps/' : './source/maps/',
+  wrapperLocation = process.env.NODE_ENV === 'custom' ? './custom/wrappers/' : './source/wrappers/',
   entryObj = {},
-  fs = require('fs'),
+  fs = require('fs-extra'),
   entries = fs.readdirSync(sourceLocation).filter(function (file) {
     return file.match(/.*\.js$/);
   }),
@@ -20,10 +21,19 @@ let IS_DEV = process.env.NODE_ENV === 'development',
 `,
   moduleWrapperFooter = `
 }));
-`;
+`,
+  wrapperTemplate,
+  mapName;
 
 entries.forEach(function (file) {
-  entryObj[file.replace(/.js/g, '')] = sourceLocation + file;
+  wrapperTemplate = `import __MAP_NAME__ from '../maps/fusioncharts.__MAP_NAME__';
+FusionCharts.addDep(__MAP_NAME__);`;
+  mapName = file.split('.')[1];
+  wrapperTemplate = wrapperTemplate.replace(/__MAP_NAME__/g, mapName);
+  fs.ensureDirSync(wrapperLocation);
+  fs.writeFileSync(wrapperLocation + file, wrapperTemplate, 'utf8');
+
+  entryObj[file.replace(/.js/g, '')] = wrapperLocation + file;
 });
 
 function getPlugins (doMinify) {
