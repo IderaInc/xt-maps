b14f8ca139768d5ffa3c1c4818d634afd8aa0db8
arnab003 <arnab.saha@fusioncharts.com>
2018-05-24 16:14:25 +0530

    RED-7547: Change build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path'),
   webpack = require('webpack'),
-  WrapperPlugin = require('wrapper-webpack-plugin');
+  WrapperPlugin = require('wrapper-webpack-plugin'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+  fs = require('fs-extra');
+
 
 let IS_DEV = process.env.NODE_ENV === 'development',
   sourceLocation = process.env.NODE_ENV === 'custom' ? './custom/maps/' : './source/maps/',
-  wrapperLocation = process.env.NODE_ENV === 'custom' ? './custom/wrappers/' : './source/wrappers/',
+  wrapperLocation = './_temp_wrappers/',
   entryObj = {},
-  fs = require('fs-extra'),
   entries = fs.readdirSync(sourceLocation).filter(function (file) {
     return file.match(/.*\.js$/);
   }),
@@ -22,76 +24,101 @@ let IS_DEV = process.env.NODE_ENV === 'development',
   moduleWrapperFooter = `
 }));
 `,
-  wrapperTemplate,
-  mapName;
+  mapName,
+  moduleObject = {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          compact: false,
+          babelrc: false,
+          cacheDirectory: true,
+          plugins: getBabelPlugins(),
+          presets: [['env', {
+            targets: { browsers: ['> 0.1%'] },
+            loose: true
+          }]]
+        }
+      }
+    }]
+  },
+  wrapperTemplateStr,
+  wrapperTemplate = `import __MAP_NAME__ from '../source/maps/fusioncharts.__MAP_NAME__';
+FusionCharts.addDep(__MAP_NAME__);`;
 
 entries.forEach(function (file) {
-  wrapperTemplate = `import __MAP_NAME__ from '../maps/fusioncharts.__MAP_NAME__';
-FusionCharts.addDep(__MAP_NAME__);`;
-  mapName = file.split('.')[1];
-  wrapperTemplate = wrapperTemplate.replace(/__MAP_NAME__/g, mapName);
+  if (process.env.NODE_ENV === 'custom') {
+    fs.copySync(sourceLocation + file, './source/maps/' + file);
+  }
+  mapName = file.match(/^fusioncharts\.(.*)\.js$/)[1];
+  wrapperTemplateStr = wrapperTemplate.replace(/__MAP_NAME__/g, mapName);
   fs.ensureDirSync(wrapperLocation);
-  fs.writeFileSync(wrapperLocation + file, wrapperTemplate, 'utf8');
+  fs.writeFileSync(wrapperLocation + file, wrapperTemplateStr, 'utf8');
 
   entryObj[file.replace(/.js/g, '')] = wrapperLocation + file;
 });
 
-function getPlugins (doMinify) {
-  var arr = [new WrapperPlugin({
+function getWebpackPlugins () {
+  let arr = [new WrapperPlugin({
     test: /\.js$/,
     header: moduleWrapperHeader,
     footer: moduleWrapperFooter
   })];
-  if (IS_DEV) {
-    return arr;
-  } else {
-    doMinify && arr.push(new webpack.optimize.UglifyJsPlugin({
+  return arr;
+};
+
+function getWebpackOptimizations () {
+  let optimization = {};
+
+  optimization.minimize = true;
+  optimization.minimizer = [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        ecma: 5,
+        ie8: true,
         mangle: false,
-        sourceMap: true,
-        mangleProperties: {
-          screw_ie8: false,
-          ignore_quoted: true
-        },
-        compress: {
-          screw_ie8: false,
-          properties: false
-        },
-        output: {
-          screw_ie8: false
-        }
-      }));
-    return arr;
-  }
-}
+        compress: false,
+        keep_classnames: true,
+        keep_fnames: true
+      }
+    })
+  ];
+  return optimization;
+};
+
+function getBabelPlugins () {
+  return [
+    ['syntax-dynamic-import'],
+    ['transform-es3-member-expression-literals'],
+    ['transform-es3-property-literals'],
+    ['transform-proto-to-assign']
+  ];
+};
 
 module.exports = [{
   entry: entryObj,
+  mode: 'none',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist', 'source')
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  },
-  plugins: getPlugins()
+  module: moduleObject,
+  plugins: getWebpackPlugins()
 },
 {
   entry: entryObj,
+  mode: 'none',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist', 'minified')
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
-  },
+  module: moduleObject,
   devtool: IS_DEV && 'source-map',
-  plugins: getPlugins(true)
+  plugins: getWebpackPlugins(),
+  optimization: getWebpackOptimizations()
 }];
